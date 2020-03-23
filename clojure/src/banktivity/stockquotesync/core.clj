bd240ac8ec6dd9d10da6bde64e56ad67adaf10b0
Jack Leow <jackgene@gmail.com>
2020-03-23 12:57:07 -0700

    Added function documentation.

--- clojure/src/banktivity/stockquotesync/core.clj
+++ clojure/src/banktivity/stockquotesync/core.clj
@@ -7,12 +7,16 @@
   (:gen-class)
   (:import (java.time LocalDate ZoneId ZoneOffset)))
 
-(defn db [db-path]
+(defn db
+  "Creates core.jdbc db-spec from the path to a SQLite file."
+  [db-path]
   (log/info (str "Processing SQLite file " db-path "..."))
   {:dbtype "sqlite"
    :dbname db-path})
 
-(defn securities [db]
+(defn securities
+  "Gets all the securities in the iBank SQLite database."
+  [db]
   (let [results (jdbc/query db ["SELECT zuniqueid AS securityid, zsymbol AS symbol FROM zsecurity"])]
     (log/info "Found" (count results) "securities...")
     results))
@@ -23,7 +27,9 @@
 (def yahoo-quote-csv-pattern
   #"Date,Open,High,Low,Close,Adj Close,Volume\n([0-9]{4}-[0-9]{2}-[0-9]{2}),([0-9]+\.[0-9]+),([0-9]+\.[0-9]+),([0-9]+\.[0-9]+),([0-9]+\.[0-9]+),[0-9]+\.[0-9]+,([0-9]+)\n?.*")
 
-(defn price [symbol]
+(defn price
+  "Gets the price information for a given security from Yahoo Finance."
+  [symbol]
   (let [resp (http/get (str "https://query1.finance.yahoo.com/v7/finance/download/" symbol "?interval=1d&events=history"))]
     (future
       (log/info "Downloading prices for " symbol "...")
@@ -41,13 +47,17 @@
              {:status 404} nil
              {:status bad-status-code} (throw (IllegalStateException. (str "Received HTTP " bad-status-code " for symbol \"" symbol "\"")))))))
 
-(defn ibank-date [date]
+(defn ibank-date
+  "Translates a standard LocalDate to its iBank representation"
+  [date]
   (.toEpochSecond
     (.atStartOfDay
       (.plusDays (.minusYears date 31) 3)
       (ZoneId/ofOffset "" (ZoneOffset/ofHours 3)))))
 
-(defn persist-price [db position]
+(defn persist-price
+  "Persists price-enriched security to the iBank SQLite database."
+  [db position]
   (let [updated (jdbc/update! db :zprice
                               {:zvolume       (:volume position)
                                :zclosingprice (:close position)
@@ -77,9 +87,7 @@
           db-spec (db db-path)
           updates (map #(persist-price db-spec %)
                        (remove (comp nil? :open)
-                               (map #(merge % @(price (:symbol %))) ; Is the deref (@) here blocking too early?
-                                    ;(comp (partial apply merge) (juxt identity (comp deref stock-quote :symbol)))
-                                    (securities db-spec))))]
+                               (map #(merge % @(price (:symbol %))) (securities db-spec))))]
       (log/info "Persisted prices for" (reduce + updates) "securities")
       (log/info "Security prices synchronized successfully."))
 
