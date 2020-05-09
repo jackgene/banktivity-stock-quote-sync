31a23be3ff809e6ff6176e01f9860c7097ef11b4
Jack Leow <jackgene@gmail.com>
2020-05-09 16:59:59 -0700

    Rewrote Go implementation to not limit HTTP concurrency using Go routines.

--- go/ibdownloadquote.go
+++ go/ibdownloadquote.go
@@ -5,6 +5,8 @@ import (
 	"encoding/csv"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/shopspring/decimal"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,7 +22,11 @@ const httpConcurrency = 4
 
 var stdOut = log.New(os.Stdout, "", log.Flags())
 var stdErr = log.New(os.Stderr, "", log.Flags())
-var iBankEpoch = time.Date(2001, time.January, 1, 0, 0, 0, 0, time.UTC)
+var appleEpoch = time.Date(2001, time.January, 1, 0, 0, 0, 0, time.UTC)
+var httpTransport = &http.Transport{
+	MaxConnsPerHost:     httpConcurrency,
+	MaxIdleConnsPerHost: httpConcurrency}
+var httpClient = &http.Client{Transport: httpTransport}
 
 type StockPrice struct {
 	securityId string
@@ -48,8 +54,8 @@ func checkDatabaseTxError(err error, tx *sql.Tx, database *sql.DB) {
 	}
 }
 
-func readSecurities(out chan *StockPrice, tx *sql.Tx, database *sql.DB) {
-	count := 0
+func readSecurities(tx *sql.Tx, database *sql.DB) []StockPrice {
+	stockPrices := make([]StockPrice, 0, 10)
 	rows, err := database.Query("SELECT zuniqueid, zsymbol FROM zsecurity")
 	checkDatabaseError(err, database)
 
@@ -59,62 +65,65 @@ func readSecurities(out chan *StockPrice, tx *sql.Tx, database *sql.DB) {
 		err := rows.Scan(&stockPrice.securityId, &stockPrice.symbol)
 		checkDatabaseTxError(err, tx, database)
 
-		out <- &stockPrice
-		count += 1
+		stockPrices = append(stockPrices, stockPrice)
 	}
-	close(out)
-	stdOut.Printf("Found %v securities...\n", count)
+	stdOut.Printf("Found %v securities...\n", len(stockPrices))
+
+	return stockPrices
 }
 
-func enrichStockPrice(in chan *StockPrice, out chan *StockPrice, tx *sql.Tx, database *sql.DB) {
-	for stockPrice := range in {
-		stdOut.Printf("Downloading prices for %v...\n", stockPrice.symbol)
-		resp, err := http.Get("https://query1.finance.yahoo.com/v7/finance/download/" + stockPrice.symbol + "?interval=1d&events=history")
+func enrichStockPrice(stockPrice StockPrice, out chan *StockPrice, tx *sql.Tx, database *sql.DB) {
+	stdOut.Printf("Downloading prices for %v...\n", stockPrice.symbol)
+	resp, err := httpClient.Get(
+		"https://query1.finance.yahoo.com/v7/finance/download/" + stockPrice.symbol + "?interval=1d&events=history")
+	checkDatabaseTxError(err, tx, database)
+
+	if resp.StatusCode == http.StatusOK {
+		csvReader := csv.NewReader(resp.Body)
+		data, err := csvReader.ReadAll()
 		checkDatabaseTxError(err, tx, database)
 
-		if resp.StatusCode == 200 {
-			csvReader := csv.NewReader(resp.Body)
-			data, err := csvReader.ReadAll()
-			checkDatabaseTxError(err, tx, database)
+		if len(data) > 1 {
+			headers := data[0]
+			if headers[0] == "Date" && headers[1] == "Open" && headers[2] == "High" &&
+				headers[3] == "Low" && headers[4] == "Close" &&
+				headers[5] == "Adj Close" && headers[6] == "Volume" {
+				cols := data[1]
+				checkDatabaseTxError(err, tx, database)
+				stockPrice.date, err = time.Parse(dateLayout, cols[0])
+				checkDatabaseTxError(err, tx, database)
+				stockPrice.open, err = decimal.NewFromString(cols[1])
+				checkDatabaseTxError(err, tx, database)
+				stockPrice.high, err = decimal.NewFromString(cols[2])
+				checkDatabaseTxError(err, tx, database)
+				stockPrice.low, err = decimal.NewFromString(cols[3])
+				checkDatabaseTxError(err, tx, database)
+				stockPrice.close, err = decimal.NewFromString(cols[4])
+				checkDatabaseTxError(err, tx, database)
+				stockPrice.volume, err = strconv.Atoi(cols[6])
+				checkDatabaseTxError(err, tx, database)
 
-			if len(data) > 1 {
-				headers := data[0]
-				if headers[0] == "Date" && headers[1] == "Open" && headers[2] == "High" &&
-					headers[3] == "Low" && headers[4] == "Close" &&
-					headers[5] == "Adj Close" && headers[6] == "Volume" {
-					cols := data[1]
-					checkDatabaseTxError(err, tx, database)
-					stockPrice.date, err = time.Parse(dateLayout, cols[0])
-					checkDatabaseTxError(err, tx, database)
-					stockPrice.open, err = decimal.NewFromString(cols[1])
-					checkDatabaseTxError(err, tx, database)
-					stockPrice.high, err = decimal.NewFromString(cols[2])
-					checkDatabaseTxError(err, tx, database)
-					stockPrice.low, err = decimal.NewFromString(cols[3])
-					checkDatabaseTxError(err, tx, database)
-					stockPrice.close, err = decimal.NewFromString(cols[4])
-					checkDatabaseTxError(err, tx, database)
-					stockPrice.volume, err = strconv.Atoi(cols[6])
-					checkDatabaseTxError(err, tx, database)
-
-					out <- stockPrice
-				}
+				out <- &stockPrice
+			} else {
+				out <- nil
 			}
-		} else if resp.StatusCode != 404 {
+		}
+	} else {
+		io.Copy(ioutil.Discard, resp.Body)
+		if resp.StatusCode != http.StatusNotFound {
 			stdErr.Printf("Got HTTP %v for %v...\n", resp.StatusCode, stockPrice.symbol)
 		}
-		resp.Body.Close()
+		out <- nil
 	}
-	out <- nil
+	resp.Body.Close()
 }
 
-func secondsSinceIBankEpoch(date time.Time) int {
-	return int(date.Add(12 * time.Hour).Sub(iBankEpoch).Seconds())
+func secondsSinceAppleEpoch(date time.Time) int {
+	return int(date.Add(12 * time.Hour).Sub(appleEpoch).Seconds())
 }
 
-func persistStockPrice(in chan *StockPrice, tx *sql.Tx, database *sql.DB) {
+func persistStockPrice(in chan *StockPrice, inputCount int, tx *sql.Tx, database *sql.DB) {
 	count := 0
-	httpWorkers := httpConcurrency
 	for stockPrice := range in {
 		if stockPrice != nil {
 			updateResult, err := tx.Exec(
@@ -129,7 +138,7 @@ func persistStockPrice(in chan *StockPrice, tx *sql.Tx, database *sql.DB) {
 					z_ent = $6 AND z_opt = $7 AND
 					zdate = $8 AND zsecurityid = $9
 				`,
-				stockPrice.volume, stockPrice.close, stockPrice.high, stockPrice.low, stockPrice.open, ent, opt, secondsSinceIBankEpoch(stockPrice.date), stockPrice.securityId)
+				stockPrice.volume, stockPrice.close, stockPrice.high, stockPrice.low, stockPrice.open, ent, opt, secondsSinceAppleEpoch(stockPrice.date), stockPrice.securityId)
 			checkDatabaseTxError(err, tx, database)
 			updateCount, err := updateResult.RowsAffected()
 			if updateCount == 0 {
@@ -140,7 +149,7 @@ func persistStockPrice(in chan *StockPrice, tx *sql.Tx, database *sql.DB) {
 					) VALUES (
 						$1, $2, $3, $4, $5, $6, $7, $8, $9
 					)`,
-					ent, opt, secondsSinceIBankEpoch(stockPrice.date), stockPrice.securityId, stockPrice.volume, stockPrice.close, stockPrice.high, stockPrice.low, stockPrice.open)
+					ent, opt, secondsSinceAppleEpoch(stockPrice.date), stockPrice.securityId, stockPrice.volume, stockPrice.close, stockPrice.high, stockPrice.low, stockPrice.open)
 				checkDatabaseTxError(err, tx, database)
 				stdOut.Printf("No existing record for %v, new row inserted...\n", stockPrice.symbol)
 
@@ -148,11 +157,10 @@ func persistStockPrice(in chan *StockPrice, tx *sql.Tx, database *sql.DB) {
 				stdOut.Printf("Existing record for %v updated...\n", stockPrice.symbol)
 			}
 			count += 1
-		} else {
-			httpWorkers -= 1
-			if httpWorkers == 0 {
-				close(in)
-			}
+		}
+		inputCount -= 1
+		if inputCount == 0 {
+			close(in)
 		}
 	}
 	_, err := tx.Exec(
@@ -176,16 +184,15 @@ func main() {
 		tx, err := database.Begin()
 		checkDatabaseTxError(err, tx, database)
 
-		enrichmentChan := make(chan *StockPrice, 1024)
 		persistenceChan := make(chan *StockPrice)
 
-		readSecurities(enrichmentChan, tx, database)
+		stockPrices := readSecurities(tx, database)
 
-		for i := 0; i < httpConcurrency; i += 1 {
-			go enrichStockPrice(enrichmentChan, persistenceChan, tx, database)
+		for _, stockPrice := range stockPrices {
+			go enrichStockPrice(stockPrice, persistenceChan, tx, database)
 		}
 
-		persistStockPrice(persistenceChan, tx, database)
+		persistStockPrice(persistenceChan, len(stockPrices), tx, database)
 
 		tx.Commit()
 		database.Close()
