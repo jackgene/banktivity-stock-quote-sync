5595bfb373273e077f0b70407899a256b5c7a1d5
Jack Leow <jackgene@gmail.com>
2025-08-31 13:40:45 -0700

    Improved Swift StockPrice representation and decoding.

--- swift/Sources/Model/StockPrice.swift
+++ swift/Sources/Model/StockPrice.swift
@@ -1,13 +1,29 @@
 import Foundation
 
 struct StockPrice: Decodable {
-    let millisSinceEpoch: Int64
+    let date: Date
     let open: Decimal
     let low: Decimal
     let high: Decimal
     let close: Decimal
     let volume: Int
-    var date: Date {
-        Date(timeIntervalSince1970: TimeInterval(millisSinceEpoch / 1000))
+    
+    enum CodingKeys: CodingKey {
+        case millisSinceEpoch
+        case open
+        case low
+        case high
+        case close
+        case volume
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.date = Date(timeIntervalSince1970: try container.decode(Double.self, forKey: .millisSinceEpoch) / 1_000)
+        self.open = try container.decode(Decimal.self, forKey: .open)
+        self.low = try container.decode(Decimal.self, forKey: .low)
+        self.high = try container.decode(Decimal.self, forKey: .high)
+        self.close = try container.decode(Decimal.self, forKey: .close)
+        self.volume = try container.decode(Int.self, forKey: .volume)
     }
 }
